fbb1b075e6c3ba375d72aa48ead8ac40787874dc
nrc4867 <nrc4867@rit.edu>
2018-10-13 14:42:27 -0400

    Changed wall jumping mechanics
    Wall jumping now blocks other movements until another wall/ground is hit

--- Unity Project Angel/Assets/Scripts/PlayerScript.cs
+++ Unity Project Angel/Assets/Scripts/PlayerScript.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : PhysicsScript {
+public class PlayerScript : PhysicsScript
+{
 
-    public bool allowMovement;
+    public bool allowInput = true;
+    /// <summary>
+    /// Mid wall jump if index 0 is true
+    /// finalize wall jump if you collide with
+    /// the opposite wall or hit the ground
+    /// </summary>
+    public bool[] wallJump = { false, false, false };
+
+    float freezeMovement;
+
     public float jumpForce, speed;
 
-    public override void Start ()
+    public override void Start()
     {
         base.Start();
-	}
-	
-	public override void Update ()
+    }
+
+    public override void Update()
     {
         base.Update();
         MovementInput();
@@ -20,24 +30,32 @@ public class PlayerScript : PhysicsScript {
 
     void MovementInput()
     {
-        if (allowMovement){
+
+        wallJump[0] = (wallJump[0]) ? !(colBottom || (wallJump[1] == colRight && wallJump[2] == colLeft)) : false;
+        if (allowInput && !wallJump[0])
+        {
             //Jump
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (colBottom) verticalVelocity = jumpForce;
-                else if (colRight)
+                if (colBottom || rightBothTouching || leftBothTouching)
+                    verticalVelocity = jumpForce; // Normal Jump
+                if (rightBothTouching && !colBottom) // Right Wall jump
                 {
                     horizontalVelocity = -speed;
-                    verticalVelocity = jumpForce;
+                    wallJump = new bool[] { true, false, true };
                     gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                    return;
                 }
-                else if (colLeft)
+                else if (leftBothTouching && !colBottom)
                 {
                     horizontalVelocity = speed;
-                    verticalVelocity = jumpForce;
+                    wallJump = new bool[] { true, true, false };
                     gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                    return;
                 }
+
             }
+
 
             //Left and Right
             if (Input.GetKey(KeyCode.A) && !colLeft)
@@ -54,5 +72,11 @@ public class PlayerScript : PhysicsScript {
             // WHYYYYYYYYY
             if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) horizontalVelocity = 0;
         }
+    }
+
+    void blockMovement(float seconds)
+    {
+        allowInput = false;
+        freezeMovement = Time.realtimeSinceStartup + seconds;
     }
 }
