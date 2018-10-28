b18b8e20c1b38daaaed9c190049d5b0caaba4844
nrc4867 <nrc4867@rit.edu>
2018-10-28 02:16:56 -0400

    Implemented a moving platform

---
+++ Unity Project Angel/Assets/Scripts/Tiles/Ground/MovingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Characters;
+using UnityEngine;
+
+namespace Assets.Scripts.Tiles.Ground
+{
+    class MovingPlatform : MonoBehaviour, IGround
+    {
+        HashSet<CharacterScript> contactingCharacters = new HashSet<CharacterScript>();
+
+        public float maxDisplacement = 3;
+        public float currentDisplacement = 0;
+        public float speed = 0.2f;
+        
+        public bool backForth = false;
+
+
+        private void Update()
+        {
+            currentDisplacement += speed * Time.deltaTime;
+            if (backForth && Math.Abs(currentDisplacement) > maxDisplacement) speed *= -1;
+            gameObject.transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            foreach(CharacterScript character in contactingCharacters)
+            {
+                character.transform.Translate(Vector2.right * speed * Time.deltaTime);
+            }
+        }
+
+        public void Part(CharacterScript character)
+        {
+            contactingCharacters.Remove(character);
+        }
+
+        public void Touch(CharacterScript character)
+        {
+            if(character.GetCollsions()[PhysicsScript.SIDES.BOTTOM].Contains(gameObject))
+            {
+                contactingCharacters.Add(character);
+            }
+        }
+    }
+}
